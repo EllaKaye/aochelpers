f48489af16dd6719521dd0d311d9feb89c298713
EllaKaye <ella.kaye@gmail.com>
2023-12-24 08:55:09 +0000

    Update examplesfor extract_numbers

--- R/string-helpers.R
+++ R/string-helpers.R
@@ -10,6 +10,7 @@
 #' @examples extract_numbers("abc123def456")
 #' @examples extract_numbers("Game 1:")
 #' @examples extract_numbers("Cards: 1 3 16 136")
+#' @examples extract_numbers("1, -3, 16, -136")
 #'
 extract_numbers <- function(x) {
 	stringr::str_extract_all(x, "-?\\d+") |>
